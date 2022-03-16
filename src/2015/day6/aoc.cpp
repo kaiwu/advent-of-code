1b4d1293fd4f5d222a13dcaa9bf0dbfe4aaaa4e6
kaiwu <kaiwu2004@gmail.com>
2022-03-16 17:30:40 +0800

    day 6

---
+++ src/2015/day6/aoc.cpp
@@ -0,0 +1,5 @@
+#include "aoc.h"
+
+namespace aoc2015 {
+int day6(line_view lv) { return 0; }
+} // namespace aoc2015
---
+++ src/2015/day6/aoc.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "common.h"
+
+namespace aoc2015 {
+
+int day6(line_view);
+
+}
