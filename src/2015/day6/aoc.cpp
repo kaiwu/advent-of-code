057a878b90cc72930a5e4fa312845fec00270a49
kaiwu <kaiwu2004@gmail.com>
2022-03-18 21:00:08 +0800

    fix tests for mac

--- src/2015/day6/aoc.cpp
+++ src/2015/day6/aoc.cpp
@@ -2,13 +2,13 @@
 
 namespace aoc2015 {
 std::pair<int, int> day6(line_view file) {
-  grid<Bit, 1000> grid1;
-  grid<int8_t, 1000> grid2;
+  grid<Bit, 1000>* grid1 = new grid<Bit,1000>;
+  grid<int8_t, 1000>* grid2 = new grid<int8_t, 1000>;
   per_line(file, [&grid1, &grid2](line_view lv) {
-    grid1.parse(lv);
-    grid2.parse(lv);
+    grid1->parse(lv);
+    grid2->parse(lv);
     return true;
   });
-  return {grid1.store_.count(), grid2.store_.count()};
+  return {grid1->store_.count(), grid2->store_.count()};
 }
 } // namespace aoc2015
