1e7f58a8c34b916b1bb7cc1155cd25db8801e284
kaiwu <kaiwu2004@gmail.com>
2022-04-03 16:48:37 +0800

    2016 day1

---
+++ test/test_2016.cpp
@@ -0,0 +1,9 @@
+#include "2016/day1/aoc.h"
+#include "catch.hpp"
+#include <stdio.h>
+
+TEST_CASE("No Time for a Taxicab", "[day1]") {
+  // line_view lv = load_file("../src/2016/day1/input");
+}
+
+
