e3d42a551d838e3fef28a132912f33740095c20b
kaiwu <kaiwu2004@gmail.com>
2022-04-04 09:59:21 +0800

    2017 day1

--- src/2017/day1/aoc.cpp
+++ src/2017/day1/aoc.cpp
@@ -1,3 +1,37 @@
 #include "aoc.h"
 
-namespace aoc2017 {}
+namespace aoc2017 {
+
+int day1(line_view lv) {
+  int sum{0};
+  const char* p1 = lv.line;
+  const char* p2 = p1 + 1;
+  while (*p2 >= '0' && *p2 <= '9') {
+    if (*p1 == *p2) {
+      sum += *p1 - '0';
+    }
+    p1++;
+    p2++;
+  }
+  if (*p1 == *lv.line) {
+    sum += *p1 - '0';
+  }
+  return sum;
+}
+
+int day1part2(line_view lv) {
+  int sum{0};
+  size_t size = lv.length % 2 == 0 ? lv.length : lv.length - 1;
+  const char* p1 = lv.line;
+  const char* p = lv.line + size / 2;
+  while (*p1 >= '0' && *p1 <= '9') {
+    const char* p2 = p1 < p ? (p + (p1 - lv.line)) : (lv.line + (p1 - p));
+    if (*p1 == *p2) {
+      sum += *p1 - '0';
+    }
+    p1++;
+  }
+  return sum;
+}
+
+} // namespace aoc2017
--- test/test_2017.cpp
+++ test/test_2017.cpp
@@ -2,6 +2,15 @@
 #include "catch.hpp"
 #include <stdio.h>
 
-TEST_CASE("", "[2017]") {
-  // line_view lv = load_file("../src/2017/day1/input");
+TEST_CASE("Inverse Captcha", "[2017]") {
+  line_view lv = load_file("../src/2017/day1/input");
+  REQUIRE(1253 == aoc2017::day1(lv));
+  REQUIRE(4 == aoc2017::day1("1111"));
+  REQUIRE(3 == aoc2017::day1("1122"));
+  REQUIRE(1278 == aoc2017::day1part2(lv));
+  REQUIRE(6 == aoc2017::day1part2("1212"));
+  REQUIRE(0 == aoc2017::day1part2("1221"));
+  REQUIRE(4 == aoc2017::day1part2("123425"));
+  REQUIRE(12 == aoc2017::day1part2("123123"));
+  REQUIRE(4 == aoc2017::day1part2("12131415"));
 }
